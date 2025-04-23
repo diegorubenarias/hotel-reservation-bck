a8becb0198c07d8d1b7c8271cbfaef4116e96afb
diego.arias <diego.arias@avature.net>
2025-04-23 18:56:40 -0300

    add room

---
+++ models/Room.model.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database'); // Adjust the path as needed
+
+const Room = sequelize.define('Room', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    roomNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    isOccupied: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+    },
+    type: {
+        type: DataTypes.ENUM('executive', 'studio', 'loft suite', 'family comfort'),
+        allowNull: false,
+    },
+}, {
+    tableName: 'rooms',
+    timestamps: true, // Adds createdAt and updatedAt fields
+});
+
+module.exports = Room;
