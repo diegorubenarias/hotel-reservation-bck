4fd0ee8cfd7cf4c9fcb38c0a095e2418e00a2090
diego.arias <diego.arias@avature.net>
2025-04-23 19:49:00 -0300

    show error

--- routes/Room.routes.js
+++ routes/Room.routes.js
@@ -16,5 +16,15 @@ router.get('/', async (req, res) => {
       res.status(500).json({ error: 'Error al obtener las rooms ' + error.message });
     }
   });
+router.post('/', async (req, res) => {
+    const { roomNumber, type, building } = req.body;
 
+    
+    try {
+        const newRoom = await Room.create({ roomNumber, type, building });
+        res.status(201).json(newRoom);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al insertar la room ' + error.message });
+    }
+});
 module.exports = router;
